e987cbac33f55a83b96ff249f648de5a360d2c34
ashishsasmal_vdesk <ashishsasmal.vdesk@gmail.com>
2021-04-26 18:31:45 +0530

    first

--- Letsmeet/asgi.py
+++ Letsmeet/asgi.py
@@ -1,16 +1,31 @@
-"""
-ASGI config for Letsmeet project.
 
-It exposes the ASGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/3.1/howto/deployment/asgi/
-"""
 
 import os
 
+from django.conf.urls import url
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Letsmeet.settings')
+# Fetch Django ASGI application early to ensure AppRegistry is populated
+# before importing consumers and AuthMiddlewareStack that may import ORM
+# models.
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Letsmeet.settings")
+django_asgi_app = get_asgi_application()
 
-application = get_asgi_application()
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
+
+import chat.routing
+
+application = ProtocolTypeRouter({
+    # Django's ASGI application to handle traditional HTTP requests
+    "http": django_asgi_app,
+
+    # WebSocket chat handler
+    'websocket': AuthMiddlewareStack(
+        URLRouter(
+            chat.routing.websocket_urlpatterns
+        )
+
+    ),
+
+})
