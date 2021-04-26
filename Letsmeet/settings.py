5feca644cbe7011ee12478598bc1b8271365eefa
ashishsasmal_vdesk <ashishsasmal.vdesk@gmail.com>
2021-04-26 18:16:28 +0530

    first

--- Letsmeet/settings.py
+++ Letsmeet/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
-
+import django_heroku
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -134,3 +134,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+django_heroku.settings(locals())
+
+LOGIN_REDIRECT_URL='chat:home'
+LOGIN_URL='chat:home'
