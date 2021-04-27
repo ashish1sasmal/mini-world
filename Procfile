25fc420dea8c4bcdb0c1361c638ea5bfb382ee42
ashishsasmal_vdesk <ashishsasmal.vdesk@gmail.com>
2021-04-27 20:27:31 +0530

    lobby

--- Procfile
+++ Procfile
@@ -1,2 +1,2 @@
-web: daphne Letsmeet.asgi:application --port $PORT --bind 0.0.0.0 -v2
-chatworker: python manage.py runworker --settings=Letsmeet.settings -v2
+web: daphne Miniworld.asgi:application --port $PORT --bind 0.0.0.0 -v2
+chatworker: python manage.py runworker --settings=Miniworld.settings -v2
--- manage.py
+++ manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Letsmeet.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Miniworld.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
